1e101d3750dbe2bc14cb2763e362d6826f4ac757
ottosven <sgotto@web.de>
2025-01-31 18:27:09 +0100

    update data documentation

--- R/fred-data.R
+++ R/fred-data.R
@@ -32,6 +32,7 @@
 #'
 #' @references
 #' * McCracken, M. W. and Ng, S. (2016) FRED-MD: A Monthly Database for Macroeconomic Research. \emph{Journal of Business & Economic Statistics}, \bold{34:4}, 574-589, \doi{10.1080/07350015.2015.1086655}.
+#'
 #' * McCracken, M. W., & Ng, S. (2020). FRED-QD: A Quarterly Database for Macroeconomic Research \bold{w26872}. National Bureau of Economic Research.
 #'
 #' @source Federal Reserve Bank of St. Louis, Michael McCracken and Serena Ng,
