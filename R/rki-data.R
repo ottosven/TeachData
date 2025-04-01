03625be989d03001a69ed0593b2e42157a570509
Sven Otto <sven.otto@uni-koeln.de>
2025-04-01 11:41:24 +0200

    add covidcases

---
+++ R/rki-data.R
@@ -0,0 +1,16 @@
+#' Reported Covid-19 infection incidence numbers Germany
+#'
+#' The Covid-19 infection incidence number is the number of reported infections in the past 7 days per 100000 inhabitants.
+#' The data are available for the individual federal states and for Germany overall.
+#'
+#' @docType data
+#'
+#' @usage data(covidcases)
+#'
+#' @format A daily multivariate time series of the class data.frame.
+#'
+#' @keywords datasets
+#'
+#' @source Robert Koch-Institut (RKI), \href{https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Kum_Tab_aktuell}{Source}, \href{https://www.govdata.de/dl-de/by-2-0}{DL-DE BY-2.0}
+"covidcases"
+
